73de0bd5bf21e487e9ad82e2f2e276147ca8f019
unknown <itzelponceespinosa@gmail.com>
2025-11-05 23:35:05 -0600

    Limpieza: confirmando archivos de IDE y hellogit2.py movidos a main

--- hellogit2.py
+++ hellogit2.py
@@ -1,1 +1,7 @@
 print("Este es mi segundo commit")
+
+print ("Holouuu mi gente Este es el primer pint agregado")
+
+print ("Hola mi gente Este es el segundo print agregado")
+
+print ("Hola mi gente Este es el segundo print agregado")
